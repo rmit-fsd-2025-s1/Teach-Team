e7682a3e65bb87645ee70a36e9fe8123b31cda1b
KavilK <s4091503@student.rmit.edu.au>
2025-05-06 15:02:46 +1000

    frontend backend

---
+++ node-express-typeorm/src/index.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import express from "express";
+import { AppDataSource } from "./data-source";
+import cors from "cors";
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) =>
+    console.log("Error during Data Source initialization:", error)
+  );

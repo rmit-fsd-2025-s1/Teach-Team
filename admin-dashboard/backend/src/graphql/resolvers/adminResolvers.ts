fb69c3c0abff793a3fa561d30913c8d1ef17a488
UmarImranShariff <s4032080@student.rmit.edu.au>
2025-06-05 23:17:37 +1000

    Only tutors to be displayed fix

--- admin-dashboard/backend/src/graphql/resolvers/adminResolvers.ts
+++ admin-dashboard/backend/src/graphql/resolvers/adminResolvers.ts
@@ -50,18 +50,21 @@ export const adminResolvers = {
       const userRepo = AppDataSource.getRepository(User);
       return userRepo.createQueryBuilder("user")
         .where("user.selectionCount >= :min", { min: 1 })
+        .andWhere("user.isLecturer = :isLecturer", { isLecturer: false })
         .getMany();
     },
     popularCandidates: async () => {
       const userRepo = AppDataSource.getRepository(User);
       return userRepo.createQueryBuilder("user")
         .where("user.selectionCount > :min", { min: 3 })
+        .andWhere("user.isLecturer = :isLecturer", { isLecturer: false })
         .getMany();
     },
     candidatesYetToBegin: async () => {
       const userRepo = AppDataSource.getRepository(User);
       return userRepo.createQueryBuilder("user")
         .where("user.selectionCount = :val", { val: 0 })
+        .andWhere("user.isLecturer = :isLecturer", { isLecturer: false })
         .getMany();
     },
   },
